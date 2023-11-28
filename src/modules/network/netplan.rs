75a071398ce9bfed5cfa096bb450d4706481f977
Paco Huelsz Prince <frhuelsz@microsoft.com>
2023-11-28 18:53:16 +0000

    Merged PR 17115: Fix silent netplan apply failure
    Related work items: #6285

--- src/modules/network/netplan.rs
+++ src/modules/network/netplan.rs
@@ -1,10 +1,9 @@
-use std::{
-    fs, io,
-    process::{Command, Output},
-};
+use std::{fs, io, process::Command};
 
 use anyhow::{Context, Error};
 use netplan_types::NetworkConfig;
+
+use osutils::exe::OutputChecker;
 
 pub const TRIDENT_NETPLAN_FILE: &str = "/etc/netplan/99-trident.yaml";
 
@@ -12,8 +11,13 @@ pub fn write(data: &str) -> io::Result<()> {
     fs::write(TRIDENT_NETPLAN_FILE, data)
 }
 
-pub fn apply() -> io::Result<Output> {
-    Command::new("/usr/sbin/netplan").args(["apply"]).output()
+pub fn apply() -> Result<(), Error> {
+    Command::new("/usr/sbin/netplan")
+        .args(["apply"])
+        .output()
+        .context("Failed to invoke `netplan apply`")?
+        .check()
+        .context("Executing `netplan apply` failed")
 }
 
 pub fn render_netplan_yaml(value: &NetworkConfig) -> Result<String, Error> {
