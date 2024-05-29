9e99cc742a550bb3e9300cc4374bdfe57d81a484
Alejandro Martinez Torres <alejandroma@microsoft.com>
2024-05-29 19:11:01 +0000

    Merged PR 19112: Bug: Checking if current version has been published
    # 🔍 Description
    
    - Modifying `get-packages.py` to be able to check if a version of a package is in the specified feed.
    - Checking if package version is already in feed before publishing.
    - Removing extra download or rpms in `download-universal-artifact.yml`
    
    # 🤔 Rationale
    
    It is not possible to publish a package with a version that is already in the feed. This generates a pipeline failure each time we run the `trident-prerelease` pipeline without any new trident rpms (`trident-prerelease` runs daily and downloads the latest trident rpms published by `trident-ci` from the universal feed).
    
    # 📝 Checks
    
    - Pipeline job that shows download latest trident rpms still works: https://dev.azure.com/mariner-org/ECF/_build/results?buildId=578123&view=logs&j=ad0591f0-650f-55a1-68a9-e9f7d7a69dff
    - Pipeline task showing you can publish when version hasn't been published yet: https://dev.azure.com/mariner-org/ECF/_build/results?buildId=578143&view=logs&j=287e5fb2-befa-5ccc-ad64-c7b19fcda332&t=eeb790f3-1dc1-5b1e-3ea0-128addb36b33&l=55
    - Pipeline task showing you can't publish when version is already in the feed: https://dev.azure.com/mariner-org/ECF/_build/results?buildId=578143&view=logs&j=f36ca166-19d0-5e18-115a-1dee7cb3a598&t=6ceea93f-e9ac-58c5-db99-d91beda48e4b&l=55
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    This pull request is a bug fix, addressing an issue where publishing fails when retesting the latest CI binary.
    
    #### PR Summary
    The changes in this pull request modify the publishing process to check if the current version has already been published, preventing redundant publishing attempts.
    - In `/scripts/get-packages.py`, the script was updated to include more command-line arguments and functionality to check if a specific version of a package is already in the feed.
    - In `/.pipelines/templates/stages/publishing/publish.yml`, a new task was added to check if the package version has already been published before attempting to publish.
    - In `/.pipelines/templates/stages/common_tasks/download-universal-artifact.yml`, the script was updated to use the new functionality of `get-packages.py` to get the latest version of a package.
    
    Related work items: #7563

--- scripts/get-packages.py
+++ scripts/get-packages.py
@@ -15,10 +15,19 @@ parser = argparse.ArgumentParser("Get package info from Devops!")
 parser.add_argument("--org", default="https://dev.azure.com/mariner-org")
 parser.add_argument("--project", default="ECF")
 parser.add_argument("--feed", default="Trident")
+parser.add_argument("--package", default="")
+parser.add_argument("--version", default="")
 
 parser.add_argument(
-    "-f", "--filter", default=None, help="Match a specific package name"
+    "--action",
+    choices=["info", "latestVersion", "isVersionInFeed"],
+    default="info",
+    help="""Defines the action to be taken. 
+                    'info' shows the information of the packages in the feed, 
+                    'latestVersion' returns the last version of the selected package, 
+                    'isVersionInFeed' checks if the specified version is in the feed for the selected package.""",
 )
+
 
 args = parser.parse_args()
 
@@ -35,6 +44,9 @@ result = subprocess.run(
         "packaging",
         "--resource",
         "packages",
+        "--query-parameters",
+        "includeAllVersions=True",
+        f"packageNameQuery={args.package}",
         "--route-parameters",
         f"project={args.project}",
         f"feedId={args.feed}",
@@ -50,15 +62,28 @@ packages = json.loads(result.stdout)["value"]
 
 output = []
 for package in packages:
-    if args.filter and args.filter != package["name"]:
-        continue
     pkgout = {
         "name": package["name"],
         "id": package["id"],
     }
 
     versions = package["versions"]
-    pkgout["latestVersion"] = versions[0]["version"] if len(versions) > 0 else None
+    pkgout["versions"] = [version_info["version"] for version_info in versions]
     output.append(pkgout)
 
-print(json.dumps(output, indent=2))
+if not output:
+    print("No matching packages found.")
+elif args.action == "info":
+    print(json.dumps(output, indent=2))
+elif not args.package:
+    print("Missing argument: package")
+elif args.action == "latestVersion":
+    print(
+        output[0]["versions"][0]
+        if len(output[0]["versions"]) > 0
+        else f"No versions of {args.package}"
+    )
+elif not args.version:
+    print("Missing argument: version")
+else:
+    print(args.version in output[0]["versions"])
