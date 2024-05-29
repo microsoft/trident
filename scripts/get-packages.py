88850ae1f26935bccd8b2b4fb4926cb69d659f0c
Paco Huelsz Prince <frhuelsz@microsoft.com>
2024-05-29 23:31:39 +0000

    Merged PR 19119: engineering: Minor improvements to get-packages.py
    # 🔍 Description
    
    * Add logging
    * Re-introduce exact name filter.
    * Only get all versions when strictly needed.
    
    # 🤔 Rationale
    
    This script may be used in more places, so I wanted to address a couple things.
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    This pull request introduces minor improvements and refactoring to the `get-packages.py` script, enhancing its functionality and readability.
    
    #### PR Summary
    The changes in this pull request primarily focus on improving the `get-packages.py` script by introducing better logging, argument parsing, and error handling. The script's functionality has been extended with the addition of new features.
    - In `get-packages.py`, the argument parsing has been improved with the addition of new arguments and the use of an Enum for action choices.
    - The script now includes better logging and error handling, making it more robust and user-friendly.
    - The logic for handling different actions (like fetching info, checking version existence, and getting the latest version) has been significantly improved and made more readable.
    - Changes in `download-universal-artifact.yml` and `publish.yml` reflect the updates made in the `get-packages.py` script, ensuring consistency across the project.
    
    Related work items: #7563

--- scripts/get-packages.py
+++ scripts/get-packages.py
@@ -8,60 +8,119 @@
 
 import subprocess
 import json
+import logging
 import argparse
+from enum import Enum
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Actions(Enum):
+    """Actions that can be taken by the script."""
+
+    INFO = "info"
+    LATEST_VERSION = "latest"
+    VERSION_EXISTS = "exists"
+
 
 parser = argparse.ArgumentParser("Get package info from Devops!")
 
 parser.add_argument("--org", default="https://dev.azure.com/mariner-org")
 parser.add_argument("--project", default="ECF")
 parser.add_argument("--feed", default="Trident")
-parser.add_argument("--package", default="")
-parser.add_argument("--version", default="")
-
+parser.add_argument("-p", "--package")
+parser.add_argument("-v", "--version")
+parser.add_argument("--debug", action="store_true")
 parser.add_argument(
+    "--all-versions", action="store_true", help="Force fetching all versions"
+)
+parser.add_argument(
+    "-a",
     "--action",
-    choices=["info", "latestVersion", "isVersionInFeed"],
-    default="info",
+    choices=list(Actions),
+    default=Actions.INFO,
+    type=Actions,
     help="""Defines the action to be taken. 
                     'info' shows the information of the packages in the feed, 
-                    'latestVersion' returns the last version of the selected package, 
-                    'isVersionInFeed' checks if the specified version is in the feed for the selected package.""",
+                    'latest' returns the last version of the selected package, 
+                    'exists' checks if the specified version is in the feed for the selected package.""",
 )
-
 
 args = parser.parse_args()
 
+# Check for required arguments
+if args.action == Actions.VERSION_EXISTS:
+    if not args.version:
+        logging.error("Missing argument: version")
+        exit(1)
+    if not args.package:
+        logging.error("Missing argument: package")
+        exit(1)
+
+# Update logging level
+logging.getLogger().setLevel(
+    logging.DEBUG if parser.parse_args().debug else logging.INFO
+)
+
+# Base command
+az_cmd = [
+    "az",
+    "devops",
+    "invoke",
+    "--org",
+    args.org,
+    "--api-version",
+    "7.1",
+    "--area",
+    "packaging",
+    "--resource",
+    "packages",
+    "--route-parameters",
+    f"project={args.project}",
+    f"feedId={args.feed}",
+    "--http-method",
+    "GET",
+]
+
+query_parameters = []
+
+# When required, query only for a specific package name *pattern*. This is a
+# pattern, not an exact match, so it does not guarantee a single package, but it
+# does narrow down the search.
+if args.package:
+    logging.info(f"Retrieving querying specific pattern: {args.package}")
+    query_parameters.append(f"packageNameQuery={args.package}")
+
+# Only retrieve all versions when needed or when explicitly requested
+if args.action == Actions.VERSION_EXISTS or args.all_versions:
+    logging.debug("Retrieving all versions")
+    query_parameters.append("includeAllVersions=True")
+
+# If we have query parameters, append them to the command
+if len(query_parameters) > 0:
+    az_cmd.append("--query-parameters")
+    az_cmd += query_parameters
+
+logging.debug(f"Running command: {' '.join(az_cmd)}")
 result = subprocess.run(
-    [
-        "az",
-        "devops",
-        "invoke",
-        "--org",
-        args.org,
-        "--api-version",
-        "7.1",
-        "--area",
-        "packaging",
-        "--resource",
-        "packages",
-        "--query-parameters",
-        "includeAllVersions=True",
-        f"packageNameQuery={args.package}",
-        "--route-parameters",
-        f"project={args.project}",
-        f"feedId={args.feed}",
-        "--http-method",
-        "GET",
-    ],
+    az_cmd,
     check=True,
     text=True,
     capture_output=True,
 )
 
+# Parse the output
 packages = json.loads(result.stdout)["value"]
 
+# Extract version information
 output = []
 for package in packages:
+    # Filter the output if a specific package was requested. The query filter is
+    # just a pattern, not an exact match, so we need to filter manually to get
+    # the exact package.
+    if args.package and args.package != package["name"]:
+        continue
+
     pkgout = {
         "name": package["name"],
         "id": package["id"],
@@ -72,18 +131,39 @@ for package in packages:
     output.append(pkgout)
 
 if not output:
-    print("No matching packages found.")
-elif args.action == "info":
+    logging.error("No packages found!")
+    exit(1)
+
+if args.action == Actions.INFO:
+    # Print the output
     print(json.dumps(output, indent=2))
-elif not args.package:
-    print("Missing argument: package")
-elif args.action == "latestVersion":
-    print(
-        output[0]["versions"][0]
-        if len(output[0]["versions"]) > 0
-        else f"No versions of {args.package}"
-    )
-elif not args.version:
-    print("Missing argument: version")
+
+elif args.action == Actions.LATEST_VERSION:
+    # Get the latest version of each package
+    latest_versions = {
+        package["name"]: (
+            package["versions"][0] if len(package["versions"]) > 0 else None
+        )
+        for package in output
+    }
+
+    # If a specific package was requested, print only that
+    if args.package:
+        latest = latest_versions.get(args.package)
+        if latest:
+            print(latest)
+        else:
+            logging.error(f"Package {args.package} has no known versions!")
+            exit(1)
+
+    # Otherwise, print all
+    else:
+        print(json.dumps(latest_versions, indent=2))
+
+elif args.action == Actions.VERSION_EXISTS:
+    # Check if the specified version is in the feed
+    print(json.dumps(args.version in output[0]["versions"]))
+
 else:
-    print(args.version in output[0]["versions"])
+    logging.error("Invalid action")
+    exit(1)
