94dd9d8832f5242b5362475137e2c77548bebb44
Nadiia Dubchak <ndubchak@microsoft.com>
2024-06-04 01:15:18 +0000

    Merged PR 19163: engineering: Add functional tests to chroot.rs
    # 🔍 Description
    
    Add functional tests to chroot.rs, including some negative test cases.
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    This pull request adds functional tests to the `chroot.rs` file in the `osutils` module.
    
    #### PR Summary
    The pull request introduces a series of functional tests to validate the behavior of the chroot environment. These tests are designed to ensure the correct functioning of the chroot environment and to catch any potential issues early.
    - In `osutils/src/chroot.rs`, a new module `functional_test` has been added which includes tests for entering and exiting the chroot, entering update chroot, entering host chroot, and negative test cases for entering chroot with failure to mount special directories and nonexistent directories.
    - The `Chroot` struct in `osutils/src/chroot.rs` has been updated to derive the `Debug` trait, which will aid in debugging and testing.
    
    Related work items: #7063

--- osutils/src/chroot.rs
+++ osutils/src/chroot.rs
@@ -17,6 +17,7 @@ use trident_api::error::{ManagementError, ReportError, TridentError, TridentResu
 /// Create a chroot environment.
 ///
 /// Note: Dropping this object does *not* exit the chroot. You must call `exit()` manually.
+#[derive(Debug)]
 pub struct Chroot {
     rootfd: RawFd,
     mounts: Vec<Mount>,
@@ -100,4 +101,178 @@ pub fn enter_update_chroot(root_mount_path: &Path) -> Result<Chroot, TridentErro
 
 pub fn enter_host_chroot(root_mount_path: &Path) -> Result<Chroot, TridentError> {
     Chroot::enter(root_mount_path, false).message("Failed to enter host chroot")
+}
+
+#[cfg(feature = "functional-test")]
+#[cfg_attr(not(test), allow(unused_imports, dead_code))]
+mod functional_test {
+    use super::*;
+    use pytest_gen::functional_test;
+    use std::fs::{self, File};
+    use tempfile::tempdir;
+    use trident_api::error::ErrorKind;
+
+    #[functional_test(feature = "helpers")]
+    fn test_enter_and_exit_chroot() {
+        // Create a temporary directory to act as the chroot environment
+        let temp_dir = tempdir().unwrap();
+        let chroot_path = temp_dir.path().to_path_buf();
+
+        // Create necessary directories for the chroot environment
+        fs::create_dir_all(chroot_path.join("dev")).unwrap();
+        fs::create_dir_all(chroot_path.join("proc")).unwrap();
+        fs::create_dir_all(chroot_path.join("sys")).unwrap();
+
+        // Create a dummy file at /
+        File::create(Path::new("/").join("dummy")).unwrap();
+        assert!(Path::new("/dummy").exists());
+
+        // Enter the chroot
+        let chroot = Chroot::enter(&chroot_path, true).unwrap();
+
+        // Verify we are inside the chroot
+        assert!(!chroot_path.join("dev").exists());
+        assert!(!chroot_path.join("proc").exists());
+        assert!(!chroot_path.join("sys").exists());
+        assert!(Path::new("/dev").exists());
+        assert!(Path::new("/proc").exists());
+        assert!(Path::new("/sys").exists());
+
+        // Verify we cannot access the dummy file from inside of chroot
+        assert!(!Path::new("/dummy").exists());
+
+        // Exit the chroot
+        chroot.exit().unwrap();
+
+        // Verify that files exist at original paths
+        assert!(chroot_path.join("dev").exists());
+        assert!(chroot_path.join("proc").exists());
+        assert!(chroot_path.join("sys").exists());
+        assert!(Path::new("/dummy").exists());
+    }
+
+    #[functional_test(feature = "helpers")]
+    fn test_enter_update_chroot() {
+        // Create a temporary directory to act as the chroot environment
+        let temp_dir = tempdir().unwrap();
+        let chroot_path = temp_dir.path().to_path_buf();
+
+        // Create necessary directories for the chroot environment
+        fs::create_dir_all(chroot_path.join("dev")).unwrap();
+        fs::create_dir_all(chroot_path.join("proc")).unwrap();
+        fs::create_dir_all(chroot_path.join("sys")).unwrap();
+
+        // Create a dummy file at /
+        File::create(Path::new("/").join("dummy")).unwrap();
+        assert!(Path::new("/dummy").exists());
+
+        // Enter the chroot
+        let chroot = enter_update_chroot(&chroot_path).unwrap();
+
+        // Verify we are inside the chroot
+        assert!(!chroot_path.join("dev").exists());
+        assert!(!chroot_path.join("proc").exists());
+        assert!(!chroot_path.join("sys").exists());
+        assert!(Path::new("/dev").exists());
+        assert!(Path::new("/proc").exists());
+        assert!(Path::new("/sys").exists());
+
+        // Verify we cannot access the dummy file from inside of chroot
+        assert!(!Path::new("/dummy").exists());
+
+        // Exit the chroot
+        chroot.exit().unwrap();
+
+        // Verify that files exist at original paths
+        assert!(chroot_path.join("dev").exists());
+        assert!(chroot_path.join("proc").exists());
+        assert!(chroot_path.join("sys").exists());
+        assert!(Path::new("/dummy").exists());
+    }
+
+    #[functional_test(feature = "helpers")]
+    fn test_enter_host_chroot() {
+        // Create a temporary directory to act as the chroot environment
+        let temp_dir = tempdir().unwrap();
+        let chroot_path = temp_dir.path().to_path_buf();
+
+        // Create a dummy file at /
+        File::create(Path::new("/").join("dummy-root")).unwrap();
+        assert!(Path::new("/dummy-root").exists());
+        // Create a dummy file in chroot_path
+        File::create(chroot_path.join("dummy-chroot")).unwrap();
+        assert!(chroot_path.join("dummy-chroot").exists());
+
+        // Enter the chroot
+        let chroot = enter_host_chroot(&chroot_path).unwrap();
+
+        // Verify we cannot access dummy-root from inside chroot, but can access dummy-chroot
+        assert!(!Path::new("/dummy-root").exists());
+        assert!(Path::new("/dummy-chroot").exists());
+
+        // Exit the chroot
+        chroot.exit().unwrap();
+
+        // Verify that files exist at the original path
+        assert!(chroot_path.join("dummy-chroot").exists());
+        assert!(Path::new("/dummy-root").exists());
+    }
+
+    #[functional_test(feature = "helpers", negative = true)]
+    fn test_enter_chroot_fail_to_mount_special_dir() {
+        // Create a temporary directory to act as the chroot environment
+        let temp_dir = tempdir().unwrap();
+        let chroot_path = temp_dir.path().to_path_buf();
+
+        // Create necessary directories for the chroot environment
+        fs::create_dir_all(chroot_path.join("dev")).unwrap();
+        fs::create_dir_all(chroot_path.join("proc")).unwrap();
+        fs::create_dir_all(chroot_path.join("sys")).unwrap();
+
+        // Pre-mount /dev to simulate failure
+        let dev_mount = Mount::builder()
+            .fstype("devtmpfs")
+            .flags(MountFlags::empty())
+            .mount("devtmpfs", chroot_path.join("dev"))
+            .unwrap();
+
+        // Attempt to enter the chroot
+        let result_dev = Chroot::enter(&chroot_path, true);
+        assert_eq!(
+            result_dev.unwrap_err().kind(),
+            &ErrorKind::Management(ManagementError::ChrootMountSpecial { dir: "/dev" })
+        );
+
+        // Un-mount /dev
+        dev_mount.unmount(UnmountFlags::empty()).unwrap();
+
+        // Pre-mount /sys to simulate failure
+        let sys_mount = Mount::builder()
+            .fstype("sysfs")
+            .flags(MountFlags::empty())
+            .mount("sysfs", chroot_path.join("sys"))
+            .unwrap();
+
+        // Attempt to enter the chroot
+        let result_sys = Chroot::enter(&chroot_path, true);
+        assert_eq!(
+            result_sys.unwrap_err().kind(),
+            &ErrorKind::Management(ManagementError::ChrootMountSpecial { dir: "/sys" })
+        );
+
+        // Un-mount /sys
+        sys_mount.unmount(UnmountFlags::empty()).unwrap();
+
+        // Mounting a new /proc filesystem over the existing one does not fail
+    }
+
+    #[functional_test(feature = "helpers", negative = true)]
+    fn test_enter_chroot_fail_nonexistent_dir() {
+        // Attempt to enter the chroot
+        let result = Chroot::enter(Path::new("/nonexistent-dir"), false);
+        assert_eq!(
+            result.unwrap_err().kind(),
+            &ErrorKind::Management(ManagementError::ChrootEnter)
+        );
+    }
 }
