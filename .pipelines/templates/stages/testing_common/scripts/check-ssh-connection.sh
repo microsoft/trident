e04228888faecf4179d8ff942e3082b170ad662e
Brian Fjeldstad <Brian.Fjeldstad@microsoft.com>
2024-12-31 22:15:34 +0000

    Merged PR 21447: engineering: speed up baremetal tests
    # 🔍 Description
    
    baremetal tests are using an exponential backoff to test when machines come back online after updates.  it looks like there is a 4 minute sleep that we are just waiting through pretty consistently because of this.  removing the exponential backoff.  also reducing the script total timeout from (1+2+4+...+2^15) seconds to 10 minutes.
    
    validated with trident-testing: https://dev.azure.com/mariner-org/ECF/_build/results?buildId=698722&view=results
    
    may not be apple-to-apple, but there does seem to be savings on a lot of the tests:
    * trident-testing successful baremetal tests time: 8hr10min
    * last few trident-prerelease successful baremetal tests times: 9hr54min, 9hr56min
    
    # 🤔 Rationale
    
    Some tests are waiting 3x for a machine to come back online.  Watching the console, it looks like a majority of the time, the machine comes back just as we start the 256 second wait.  Potentially, we could save close to 12 minutes on any of those tests.
    
    Also, i suspect having an exponential wait time that gets to 2^15 seconds is incompatible with any test timeout we have.
    
    # 📝 Checks
    
    - [ ] Check [dev-docs/manual-validation.md](/dev-docs/manual-validation.md)
    
    # 📌 Follow-ups
    
    TODO:
    
    - #0000
    
    # 🗒️ Notes
    
    make check-ssh-connection more responsive to speed up tests
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Code optimization to speed up baremetal tests.
    
    #### PR Summary
    This pull request optimizes the SSH connection retry logic to speed up baremetal tests.
    - `.pipelines/templates/stages/testing_common/scripts/check-ssh-connection.sh`: Changed the retry wait time to a fixed 5 seconds and set a total wait time of 10 minutes, removing the exponential backoff logic.
    
    Related work items: #10355

--- .pipelines/templates/stages/testing_common/scripts/check-ssh-connection.sh
+++ .pipelines/templates/stages/testing_common/scripts/check-ssh-connection.sh
@@ -7,24 +7,45 @@ set -eux
 ssh_key_path=$1
 user_name=$2
 host_ip=$3
+runtime_env=$4
+
+WAIT_TRIDENT_SERVICE_FINISHED=1
+if [[ "$runtime_env" == "container" ]]; then
+    WAIT_TRIDENT_SERVICE_FINISHED=0
+fi
 
 ls -l "$ssh_key_path"
 sshSucceeded=false
-waitTime=1  # Initial wait time set to 1 second
+waitTimeSeconds=5  # Retry wait time set to 5 seconds
 
-# Check SSH connection and exponentially increase wait time if it fails,
-# so that the total wait time is 5+ minutes
-for i in {1..15}; do
-    if timeout 15 ssh -q -o "ConnectTimeout=10" -o "StrictHostKeyChecking=no" -i "$ssh_key_path" "${user_name}@${host_ip}" exit; then
+SSH_CMD_ARGS="-o StrictHostKeyChecking=no -i $ssh_key_path ${user_name}@${host_ip}"
+
+# Check SSH connection so that the total wait time is 10 minutes
+start_time=$(date +%s)
+end_time=$((start_time + 600))  # 10 minutes = 600 seconds
+
+while [ $(date +%s) -lt $end_time ]; do
+    if timeout 15 ssh -q -o "ConnectTimeout=10" $SSH_CMD_ARGS exit; then
         echo "SSH connection successful. Host is up and running."
-        sshSucceeded=true
-        break
-    else
-        echo "SSH connection failed. Waiting for $waitTime seconds before retrying..."
-        sleep $waitTime
-        # Double the wait time for the next iteration
-        waitTime=$((waitTime * 2))
+        if [[ "$WAIT_TRIDENT_SERVICE_FINISHED" == "1" ]]; then
+            echo "Check for trident.service completion."
+            set +e
+            trident_status=$(ssh -q $SSH_CMD_ARGS sudo systemctl status trident.service --no-pager)
+            echo -e ".... check trident services:\n$trident_status\n"
+            set -e
+            if echo $trident_status | grep "Loaded: loaded" -A 5 | grep "Active:" | grep "dead"; then
+                sshSucceeded=true
+                break
+            fi
+        else
+            echo "Do not wait for trident.service completion."
+            sshSucceeded=true
+            break
+        fi
     fi
+
+    echo "Waiting for $waitTimeSeconds seconds before retrying..."
+    sleep $waitTimeSeconds
 done
 
 # If SSH connection did not succeed, fail
