f725f8834a0c6fb7e5bc4e5d80fe49165510e2a7
Jiri Appl <Jiri.Appl@microsoft.com>
2025-02-26 23:32:35 +0000

    Merged PR 22214: Timeout on cleanup
    # 🔍 Description
    
    When the VM fails to boot, the cleanup task stalls, as it cannot connect to the VM to fetch the logs. Time PR adds a timeout on the clean up task, so the pipeline run can complete faster. In addition, this PR fixes the cleanup logic, as sudo was required.
    
    # 🤔 Rationale
    
    Reduce e2e time when VM fails to boot.
    
    # 🗒️ Notes
    
    Timeout on cleanup
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Bug fix
    
    #### PR Summary
    This pull request addresses a bug related to cleanup operations and adds a timeout to a testing stage.
    - `scripts/loop-update/cleanup-vm.sh`: Added `sudo` to `virsh` commands for proper permission handling.
    - `/.pipelines/templates/stages/testing_servicing/testing-template.yml`: Added a 5-minute timeout to the "Publish OS disk" step.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Related work items: #11190

--- scripts/loop-update/cleanup-vm.sh
+++ scripts/loop-update/cleanup-vm.sh
@@ -9,8 +9,8 @@ if [ "$TEST_PLATFORM" == "azure" ]; then
         az group delete -n $TEST_RESOURCE_GROUP -y
     fi
 elif [ "$TEST_PLATFORM" == "qemu" ]; then
-    virsh destroy $VM_NAME || true
-    virsh undefine $VM_NAME --nvram || true
+    sudo virsh destroy $VM_NAME || true
+    sudo virsh undefine $VM_NAME --nvram || true
 fi
 killUpdateServer $UPDATE_PORT_A
 killUpdateServer $UPDATE_PORT_B
