b59a3b4fb797c24398088ad5fff5aab5847f537d
Jiri Appl <Jiri.Appl@microsoft.com>
2025-02-28 20:26:47 +0000

    Merged PR 22269: Diagnose multi IP issue on QEMU
    # 🔍 Description
    
    Qemu VMs sometimes get multiple IPs; we dont know yet why. This change will allow us to fail with more explicit error and diagnose the issue further.
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Bug fix
    
    #### PR Summary
    This pull request addresses an issue where QEMU VMs sometimes return multiple IP addresses, which is not handled correctly.
    - `scripts/loop-update/check-deployment.sh`: Added logic to diagnose and explicitly fail if multiple IPs are found, aiding in identifying the root cause of the issue.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Related work items: #11273

--- scripts/loop-update/check-deployment.sh
+++ scripts/loop-update/check-deployment.sh
@@ -4,4 +4,17 @@ set -euxo pipefail
 . $(dirname $0)/common.sh
 
 VM_IP=`getIp`
+
+# Help diagnose https://dev.azure.com/mariner-org/ECF/_workitems/edit/11273 and
+# fail explicitly if multiple IPs are found
+if [ "$TEST_PLATFORM" == "qemu" ]; then
+    if [ `echo $VM_IP | wc -l` -gt 1 ]; then
+        echo "Multiple IPs found:"
+        echo $VM_IP
+        sudo virsh domifaddr $VM_NAME 
+        adoError "Multiple IPs found"
+        exit 1
+    fi
+fi
+
 checkActiveVolume "volume-a" 0
