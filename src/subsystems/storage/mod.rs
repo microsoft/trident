d5a2d328cd1e23520bd53c603fb2f24a2755b7cf
Nadiia Dubchak <ndubchak@microsoft.com>
2025-06-19 23:34:51 +0000

    Updated post rebase

--- src/subsystems/storage/mod.rs
+++ src/subsystems/storage/mod.rs
@@ -175,7 +175,7 @@ impl Subsystem for StorageSubsystem {
 
         // If this is a UKI image AND we're performing a clean install, then we need to run the
         // encryption provision logic, i.e. re-seal the encryption key to pcrlock policy!
-        if ctx.spec.internal_params.get_flag(ENABLE_UKI_SUPPORT) {
+        if ctx.is_uki_image()? {
             debug!("Starting step 'Provision' for subunit '{ENCRYPTION_SUBSYSTEM_NAME}'");
             encryption::provision(ctx, mount_path).message(format!(
                 "Step 'Provision' failed for subunit '{ENCRYPTION_SUBSYSTEM_NAME}'"
