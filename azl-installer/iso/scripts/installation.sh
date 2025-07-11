4b36a3229d72db7e40bdf23f91c3d1dd88a6a6e5
Alejandro Martinez Torres <alejandroma@microsoft.com>
2025-07-11 21:13:24 +0000

    Provide password script with path instead of content

--- azl-installer/iso/scripts/installation.sh
+++ azl-installer/iso/scripts/installation.sh
@@ -5,6 +5,8 @@ trap '/bin/bash' ERR
 CD_INSTALLER_DIR="/mnt/trident_cdrom/installer"
 WORKING_DIR="/root/installer"
 TRIDENT_CONFIG="/etc/trident/config.yaml"
+TRIDENT_SCRIPTS="/etc/trident/scripts"
+TRIDENT_PASSWORD_SCRIPT="$TRIDENT_SCRIPTS/user-password.sh"
 
 cp -r "$CD_INSTALLER_DIR/" "/root/"
 
@@ -18,16 +20,23 @@ cd "$WORKING_DIR"
   --log-level=trace \
   --log-file=$WORKING_DIR/liveinstaller.log > "$WORKING_DIR/output_liveinstaller.log" 2>&1
 
-
+# Update device path in Trident's Host Configuration with user input
 DISK=$(jq -r .disk_path "$WORKING_DIR/userinput.json")
-HOSTNAME=$(jq -r .hostname "$WORKING_DIR/userinput.json")
-USERNAME=$(jq -r .username "$WORKING_DIR/userinput.json")
-USERPASSWORD=$(jq -r .password "$WORKING_DIR/userinput.json")
-
 sed -i "s|__DISK_PATH__|$DISK|g" "$TRIDENT_CONFIG"
+
+# Update hostname in Trident's Host Configuration with user input
+HOSTNAME=$(jq -r .hostname "$WORKING_DIR/userinput.json")
 sed -i "s|__HOST_NAME__|$HOSTNAME|g" "$TRIDENT_CONFIG"
+
+# Update username in Trident's Host Configuration with user input
+USERNAME=$(jq -r .username "$WORKING_DIR/userinput.json")
 sed -i "s|__USER_NAME__|$USERNAME|g" "$TRIDENT_CONFIG"
-sed -i "s|__USER_PASSWORD__|$USERPASSWORD|g" "$TRIDENT_CONFIG"
+
+# Create user-password.sh script to set the password for the user
+mkdir -p $TRIDENT_SCRIPTS
+USERPASSWORD=$(jq -r .password "$WORKING_DIR/userinput.json")
+echo "echo '$USERNAME:$USERPASSWORD' | chpasswd" > $TRIDENT_PASSWORD_SCRIPT
+chmod 700 $TRIDENT_PASSWORD_SCRIPT
 
 /bin/trident install
 /bin/bash
