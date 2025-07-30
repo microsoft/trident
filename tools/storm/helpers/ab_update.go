af6c2d40373214171fb9d48bc5e8472652050b75
Ayana Yaegashi <ayaegashi@microsoft.com>
2025-07-30 19:24:28 +0000

    fix go again

--- tools/storm/helpers/ab_update.go
+++ tools/storm/helpers/ab_update.go
@@ -112,24 +112,24 @@ func (h *AbUpdateHelper) updateHostConfig(tc storm.TestCase) error {
 
 	logrus.Debugf("Base name: %s", base)
 
-	// Match form <name>_v<version number>.<file extension> (note that "_v<version number>" is optional)
-	matches := regexp.MustCompile(`^(.*?)(_v\d+)?\.(.+)$`).FindStringSubmatch(base)
-	logrus.Debugf("File pattern matches: %+v (length: %d)", matches, len(matches))
-
 	// Match form <repository>:v<build ID>.<version number>
 	matches_oci := regexp.MustCompile(`^(.+):v(\d+)\.(\d+)$`).FindStringSubmatch(base)
 	logrus.Debugf("File pattern matches: %+v (length: %d)", matches_oci, len(matches_oci))
 
+	// Match form <name>_v<version number>.<file extension> (note that "_v<version number>" is optional)
+	matches := regexp.MustCompile(`^(.*?)(_v\d+)?\.(.+)$`).FindStringSubmatch(base)
+	logrus.Debugf("File pattern matches: %+v (length: %d)", matches, len(matches))
+
 	var newCosiName string
 
-	if len(matches) == 4 {
-		name := matches[1]
-		ext := matches[3]
-		newCosiName = fmt.Sprintf("%s_v%s.%s", name, h.args.Version, ext)
-	} else if len(matches_oci) == 4 {
+	if strings.HasPrefix(oldUrl, "oci://") {
 		name := matches_oci[1]
 		buildId := matches_oci[2]
 		newCosiName = fmt.Sprintf("%s:v%s.%s", name, buildId, h.args.Version)
+	} else if len(matches) == 4 {
+		name := matches[1]
+		ext := matches[3]
+		newCosiName = fmt.Sprintf("%s_v%s.%s", name, h.args.Version, ext)
 	} else {
 		return fmt.Errorf("failed to parse image name: %s", base)
 	}
