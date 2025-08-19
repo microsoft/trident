563f405316e83df0d2eaddbcb988948837018d5b
Brian Fjeldstad <bfjelds@microsoft.com>
2025-08-19 17:13:26 +0000

    use private location

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,10 +20,11 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://microsoft.github.io',
+  // url: 'https://microsoft.github.io',
+  url: 'https://vigilant-adventure-5jnm363.pages.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/trident/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
