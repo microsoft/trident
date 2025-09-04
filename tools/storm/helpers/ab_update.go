ef13ba365e95ee45d561d6f01d7d394f4d16a16d
Brian Fjeldstad <bfjelds@microsoft.com>
2025-09-04 19:42:13 +0000

    comment

--- tools/storm/helpers/ab_update.go
+++ tools/storm/helpers/ab_update.go
@@ -291,7 +291,7 @@ func (h *AbUpdateHelper) checkTridentService(tc storm.TestCase) error {
 			logrus.Infof("SSH dial to '%s' succeeded", h.args.SshCliSettings.FullHost())
 
 			if h.args.ForcedRollback {
-				logrus.Infof("Forced rollback is expected")
+				logrus.Infof("Forced rollback is expected, skip service check as commit should fail")
 				// Check for rollback
 				out, err := utils.InvokeTrident(h.args.Env, client, h.args.Proxy, "get status")
 				if err != nil {
@@ -308,7 +308,6 @@ func (h *AbUpdateHelper) checkTridentService(tc storm.TestCase) error {
 				if err != nil {
 					return nil, fmt.Errorf("failed to unmarshal YAML: %w", err)
 				}
-
 				return nil, nil
 			}
 
