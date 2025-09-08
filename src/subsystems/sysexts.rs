ed71c305f6465a2b004689e6f3fd26b4eaae5cc9
Ayana Yaegashi <ayaegashi@microsoft.com>
2025-09-08 18:59:10 +0000

    clean install works

--- src/subsystems/sysexts.rs
+++ src/subsystems/sysexts.rs
@@ -1,8 +1,8 @@
-use std::{fs, path::Path};
+use std::{fs, os::unix::fs as fs_unix, path::Path};
 
-use log::debug;
+use log::{debug, error};
 
-use osutils::osmodifier::OSModifierConfig;
+use osutils::{osmodifier::OSModifierConfig, path};
 use trident_api::{
     config::Services,
     error::{InternalError, ReportError, ServicingError, TridentError},
@@ -28,15 +28,16 @@ impl Subsystem for SysextsSubsystem {
             debug!("No sysexts found in HC. Returning early.");
             return Ok(());
         };
-        debug!("Found the following sysexts from the HC: {sysexts:?}");
+        debug!("validate: Found the following sysexts from the HC: {sysexts:?}");
 
         // Ensure that all sysexts are *.raw files.
-        if sysexts
+        if let Some(sysext) = sysexts
             .add
             .clone()
             .into_iter()
-            .any(|sysext| sysext.url.to_string().ends_with(".raw"))
+            .find(|sysext| !sysext.url.to_string().ends_with(".raw"))
         {
+            error!("Invalid sysext: {:?}", sysext.url);
             return Err(TridentError::internal("Invalid sysext received"));
         };
 
@@ -44,11 +45,42 @@ impl Subsystem for SysextsSubsystem {
     }
 
     // Outside of chroot
-    // fn provision(&mut self, ctx: &EngineContext, mount_path: &Path) -> Result<(), TridentError> {
-    //     // Check for existing sysexts on the system
+    fn provision(&mut self, ctx: &EngineContext, mount_path: &Path) -> Result<(), TridentError> {
+        let Some(sysexts) = &ctx.spec.sysexts else {
+            debug!("No sysexts found in HC. Returning early.");
+            return Ok(());
+        };
+        debug!("provision: Found the following sysexts from the HC: {sysexts:?}");
 
-    //     Ok(())
-    // }
+        // Create directory for sysexts in shared partition if it doesn't exist already
+        let provisioned_os_shared_partition_path =
+            path::join_relative(mount_path, SHARED_PARTITION_PATH);
+        fs::create_dir_all(provisioned_os_shared_partition_path.join("extensions")).structured(
+            InternalError::Internal(
+                "failed to create directory for extensions in shared partition",
+            ),
+        )?;
+
+        // Move the sysext files from the MOS to the ROS
+        for sysext in &sysexts.add {
+            let current_file_path = sysext
+                .url
+                .to_file_path()
+                .unwrap_or_default()
+                .display()
+                .to_string();
+            let sysext_file_name = &current_file_path.split("/").last().unwrap_or_default();
+            let new_file_path = &provisioned_os_shared_partition_path
+                .join("extensions")
+                .join(sysext_file_name);
+            debug!("Attempting to move sysext from {current_file_path} to {new_file_path:?}");
+            fs::copy(&current_file_path, new_file_path).structured(InternalError::Internal(
+                "Failed to move sysext to the directory for sysexts",
+            ))?;
+        }
+
+        Ok(())
+    }
 
     // Inside chroot
     fn configure(&mut self, ctx: &EngineContext) -> Result<(), TridentError> {
@@ -56,29 +88,40 @@ impl Subsystem for SysextsSubsystem {
             debug!("No sysexts found in HC. Returning early.");
             return Ok(());
         };
-        debug!("Found the following sysexts from the HC: {sysexts:?}");
+        debug!("configure: Found the following sysexts from the HC: {sysexts:?}");
 
-        // Create directory for sysexts if it doesn't exist already
+        // Create directory for sysexts in shared partition if it doesn't exist already
         fs::create_dir_all(Path::new(SHARED_PARTITION_PATH).join("extensions")).structured(
             InternalError::Internal(
                 "failed to create directory for extensions in shared partition",
             ),
         )?;
 
+        // Create directory for sysexts in /var/lib/extensions if it doesn't exist already
+        debug!("Ensure /var/lib/extensions exists");
+        fs::create_dir_all("/var/lib/extensions").structured(InternalError::Internal(
+            "failed to create directory for extensions in newroot at /var/lib/extensions",
+        ))?;
+
         // Place sysexts in shared partition
         for sysext in &sysexts.add {
-            fs::rename(
-                sysext.url,
-                Path::new(SHARED_PARTITION_PATH)
-                    .join("extensions")
-                    .join("placeholder"),
-            )
-            .structured(InternalError::Internal(
-                "Failed to move sysext to the directory for sysexts",
-            ))?;
-        }
+            let current_file_path = sysext
+                .url
+                .to_file_path()
+                .unwrap_or_default()
+                .display()
+                .to_string();
+            let sysext_file_name = &current_file_path.split("/").last().unwrap_or_default();
+            let new_file_path = Path::new(SHARED_PARTITION_PATH)
+                .join("extensions")
+                .join(sysext_file_name);
+            debug!("Attempting to move sysext from {current_file_path} to {new_file_path:?}");
 
-        // Symlink extensions to /var/lib/extensions
+            let symlink_path = Path::new("/var/lib/extensions").join(sysext_file_name);
+            debug!("Add symlink from {new_file_path:?} to {symlink_path:?}");
+            fs_unix::symlink(&new_file_path, symlink_path)
+                .structured(InternalError::Internal("Failed to make symlink"))?;
+        }
 
         // Call OS Modifier to enable systemd-sysext
         let os_modifier_config = OSModifierConfig {
