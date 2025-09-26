21362953c7d97dd3a3cbec32572578b2ae843b96
bfjelds <bfjelds@microsoft.com>
2025-09-26 15:33:12 -0700

    migrate to new setting (#193)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -76,7 +76,12 @@ const config = {
   projectName: 'trident', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'throw',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
