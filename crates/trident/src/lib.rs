ad3ef331f900b3e0d415e761672b273c4f5f9e5f
Ayana Yaegashi <ayaegashi@microsoft.com>
2025-10-01 20:42:43 +0000

    omit extensions in context for rebuild raid

--- crates/trident/src/lib.rs
+++ crates/trident/src/lib.rs
@@ -366,6 +366,8 @@ impl Trident {
             storage_graph: engine::build_storage_graph(&host_config.storage)?, // Build storage graph
             filesystems: Vec::new(), // Left empty since context does not have image
             is_uki: None,
+            extensions: Vec::new(),
+            extensions_old: Vec::new(),
         };
 
         if ctx.ab_active_volume.is_none() {
