e357d74b315c400b8d8aa8bfd72d0045e1f29bcc
Alejandro Martinez Torres <alejandroma@microsoft.com>
2025-10-15 17:11:24 +0000

    Fix mount issue

--- tests/images/azl-installer/iso/scripts/installation.sh
+++ tests/images/azl-installer/iso/scripts/installation.sh
@@ -2,20 +2,21 @@
 set -ex
 trap '/bin/bash' ERR
 
+# Mount CD-ROM using symlink
+mkdir -p /mnt/cdrom
+mount /dev/cdrom /mnt/cdrom
 
 INSTALLER_DIR="/mnt/cdrom/installer/"
+IMAGES_ISO_DIR="/mnt/cdrom/images/"
 TRIDENT_CONFIG="/etc/trident/config.yaml"
-
-# Copy the installer files to the working directory (merge with existing)
-cp -r "$INSTALLER_DIR"* "/root/installer/"
-
-# Copy images from ISO to working directory
-mkdir -p "/root/installer/images"
-cp /mnt/cdrom/images/azure-linux-trident.cosi /root/installer/images/
-cp /mnt/cdrom/images/azure-linux-full.cosi /root/installer/images/
-
 WORKING_DIR="/root/installer"
 IMAGES_DIR="$WORKING_DIR/images/"
+
+# Copy the installer files to the working directory (merge with existing)
+cp -r "$INSTALLER_DIR"* "$WORKING_DIR"
+
+# Copy images from ISO to working directory
+cp -r "$IMAGES_ISO_DIR" "$WORKING_DIR"
 
 cd "$WORKING_DIR"
 "$WORKING_DIR/liveinstaller" \
--- tests/images/azl-installer/mos/scripts/mount-cdrom.sh
+++
@@ -1,3 +0,0 @@
-# Mount CD-ROM using symlink
-mkdir -p /mnt/cdrom
-mount /dev/cdrom /mnt/cdrom
