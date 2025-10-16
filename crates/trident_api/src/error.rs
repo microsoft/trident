388f5a54d7591df2ae4198a2cf81a0f94dcd3ebe
Ayana Yaegashi <ayaegashi@microsoft.com>
2025-10-16 18:30:41 +0000

    fixed typo

--- crates/trident_api/src/error.rs
+++ crates/trident_api/src/error.rs
@@ -100,7 +100,7 @@ pub enum InternalError {
     #[error("Encountered a panic: {0}")]
     Panic(String),
 
-    #[error("Failed to prepare all sysexts and confexts from the Host Configurations")]
+    #[error("Failed to prepare all sysexts and confexts from the Host Configuration")]
     PopulateExtensionImages,
 
     #[error("Failed to populate filesystems vector in Engine Context: {0}")]
