8f466e1adb2f86212cd4661dedadb3ef86a5f7e1
Ayana Yaegashi <ayaegashi@microsoft.com>
2025-11-04 04:00:26 +0000

    use host status

--- tests/e2e_tests/extensions_test.py
+++ tests/e2e_tests/extensions_test.py
@@ -1,25 +1,29 @@
 import fabric
 import pytest
 
+from base_test import get_host_status
+
 pytestmark = [pytest.mark.extensions]
 
 
 def test_extensions(
     connection: fabric.Connection,
-    hostConfiguration: dict,
+    tridentCommand: str,
 ) -> None:
-    osConfig = hostConfiguration["os"]
+    hostStatus = get_host_status(connection, tridentCommand)
+    hostConfig = hostStatus["spec"]
+    osConfig = hostConfig["os"]
     print(osConfig)
     sysextsConfig = osConfig["sysexts"]
     for sysext in sysextsConfig:
         # Verify that the path exists on the target OS
         path = sysext["path"]
-        result = connection.run(f"test -e path {path}", warn=True)
+        result = connection.run(f"test -e {path}", warn=True)
         assert result.ok, f"sysext path does not exist: {path}"
 
     confextsConfig = osConfig["confexts"]
     for confext in confextsConfig:
         # Verify that the path exists on the target OS
         path = confext["path"]
-        result = connection.run(f"test -e path {path}", warn=True)
+        result = connection.run(f"test -e {path}", warn=True)
         assert result.ok, f"confext path does not exist: {path}"
