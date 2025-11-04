53e9e1578a5942ceb8597e9da5c390eb52caeb96
Ayana Yaegashi <ayaegashi@microsoft.com>
2025-11-04 05:06:53 +0000

    fix

--- tests/e2e_tests/extensions_test.py
+++ tests/e2e_tests/extensions_test.py
@@ -13,17 +13,18 @@ def test_extensions(
     hostStatus = get_host_status(connection, tridentCommand)
     hostConfig = hostStatus["spec"]
     osConfig = hostConfig["os"]
-    print(osConfig)
-    sysextsConfig = osConfig["sysexts"]
-    for sysext in sysextsConfig:
-        # Verify that the path exists on the target OS
-        path = sysext["path"]
-        result = connection.run(f"test -e {path}", warn=True)
-        assert result.ok, f"sysext path does not exist: {path}"
 
-    confextsConfig = osConfig["confexts"]
-    for confext in confextsConfig:
-        # Verify that the path exists on the target OS
-        path = confext["path"]
-        result = connection.run(f"test -e {path}", warn=True)
-        assert result.ok, f"confext path does not exist: {path}"
+    if "sysexts" in osConfig:
+        sysextsConfig = osConfig["sysexts"]
+        for sysext in sysextsConfig:
+            # Verify that the path exists on the target OS
+            path = sysext["path"]
+            result = connection.run(f"test -e {path}", warn=True)
+            assert result.ok, f"sysext path does not exist: {path}"
+    if "confexts" in osConfig:
+        confextsConfig = osConfig["confexts"]
+        for confext in confextsConfig:
+            # Verify that the path exists on the target OS
+            path = confext["path"]
+            result = connection.run(f"test -e {path}", warn=True)
+            assert result.ok, f"confext path does not exist: {path}"
