c1a3657102cd1f6da385080ff5eeff05903437e3
Jonathan Behrens <jbehrens@microsoft.com>
2025-11-05 14:41:54 -0800

    Update /etc/fstab and grub.cfg to replace part uuids and labels

--- crates/trident/src/engine/boot/mod.rs
+++ crates/trident/src/engine/boot/mod.rs
@@ -1,11 +1,11 @@
-use std::path::Path;
+use std::{fs, path::Path};
 
-use log::debug;
+use log::{debug, trace, warn};
 use strum::IntoEnumIterator;
 
 use trident_api::{
     constants::{AB_VOLUME_A_NAME, AB_VOLUME_B_NAME, AZURE_LINUX_INSTALL_ID_PREFIX, VAR_TMP_PATH},
-    error::{ReportError, ServicingError, TridentError},
+    error::{InternalError, ReportError, ServicingError, TridentError, TridentResultExt},
     status::AbVolumeSelection,
 };
 
@@ -32,12 +32,44 @@ impl Subsystem for BootSubsystem {
             return Ok(());
         }
         if ctx.spec.storage.raw_cosi {
-            debug!("Skipping grub configuration because raw COSI is in use");
-            return Ok(());
-        }
+            let paths = [
+                Path::new("/boot/grub/grub.cfg"),
+                Path::new("/boot/grub2/grub.cfg"),
+            ];
 
-        grub::update_configs(ctx, Path::new(OS_MODIFIER_NEWROOT_PATH))
-            .structured(ServicingError::UpdateGrubConfigs)?;
+            let Some(path) = paths.into_iter().find(|p| p.exists()) else {
+                debug!("No grub.cfg found on /boot. Skipping update");
+                return Ok(());
+            };
+
+            let Ok(grub_cfg) = fs::read_to_string(path) else {
+                debug!(
+                    "Failed to read grub.cfg from /boot/grub, skipping raw COSI grub configuration"
+                );
+                return Ok(());
+            };
+
+            let fs_uuid = &ctx
+                .image
+                .as_ref()
+                .structured(InternalError::Internal("No image found in engine context"))?
+                .root_filesystem()
+                .structured(InternalError::Internal("No root filesystem in image"))?
+                .fs_uuid;
+
+            let regex =
+                regex::Regex::new(r"\broot=((PARTUUID|PARTLABEL)=([a-fA-F0-9\-]{36}))\b").unwrap();
+            let updated = regex
+                .replace_all(&grub_cfg, format!("root=UUID={fs_uuid}").as_str())
+                .to_string();
+            trace!("Updated grub.cfg:\n{}", &updated);
+            fs::write(path, updated)
+                .structured(ServicingError::UpdateGrubConfigs)
+                .message("Failed to write updated grub.cfg for raw COSI")?;
+        } else {
+            grub::update_configs(ctx, Path::new(OS_MODIFIER_NEWROOT_PATH))
+                .structured(ServicingError::UpdateGrubConfigs)?;
+        }
 
         Ok(())
     }
