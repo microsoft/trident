d3a2b732f2117f8e952e0c9b6c994c74d0416065
Ayana Yaegashi <ayaegashi@microsoft.com>
2025-11-05 18:04:34 +0000

    feedback

--- tests/e2e_tests/extensions_test.py
+++ tests/e2e_tests/extensions_test.py
@@ -15,39 +15,22 @@ def test_extensions(
     hostConfig = hostStatus["spec"]
     osConfig = hostConfig["os"]
 
-    if "sysexts" in osConfig:
-        sysextsConfig = osConfig["sysexts"]
+    for extType in ["sysext", "confext"]:
+        configExtType = f"{extType}s"
+        if configExtType in osConfig:
+            extConfig = osConfig[configExtType]
+            result = connection.run(
+                f"sudo systemd-{extType} list --json=pretty --no-pager", warn=True
+            )
+            assert result.ok, f"failed to run 'systemd-{extType} list': {result.stderr}"
+            ext_list = json.loads(result.stdout)
 
-        result = connection.run(
-            "sudo systemd-sysext list --json=pretty --no-pager", warn=True
-        )
-        assert result.ok, f"failed to run 'systemd-sysext list': {result.stderr}"
-        sysext_list = json.loads(result.stdout)
+            for ext in extConfig:
+                # Verify that the path exists on the target OS
+                path = ext["path"]
+                result = connection.run(f"test -e {path}", warn=True)
+                assert result.ok, f"{extType} path does not exist: {path}"
 
-        for sysext in sysextsConfig:
-            # Verify that the path exists on the target OS
-            path = sysext["path"]
-            result = connection.run(f"test -e {path}", warn=True)
-            assert result.ok, f"sysext path does not exist: {path}"
-
-            # Extract filename from path and check if it's in systemd-sysext list
-            found = any(ext.get("path") == path for ext in sysext_list)
-            assert found, f"sysext at {path} not found in systemd-sysext list"
-    if "confexts" in osConfig:
-        confextsConfig = osConfig["confexts"]
-
-        result = connection.run(
-            "sudo systemd-confext list --json=pretty --no-pager", warn=True
-        )
-        assert result.ok, f"failed to run 'systemd-confext list': {result.stderr}"
-        confext_list = json.loads(result.stdout)
-
-        for confext in confextsConfig:
-            # Verify that the path exists on the target OS
-            path = confext["path"]
-            result = connection.run(f"test -e {path}", warn=True)
-            assert result.ok, f"confext path does not exist: {path}"
-
-            # Extract filename from path and check if it's in systemd-confext list
-            found = any(ext.get("path") == path for ext in confext_list)
-            assert found, f"confext at {path} not found in systemd-confext list"
+                # Extract filename from path and check if it's in systemd-*ext list
+                found = any(e.get("path") == path for e in ext_list)
+                assert found, f"{extType} at {path} not found in systemd-{extType} list"
