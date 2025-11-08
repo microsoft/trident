cd638bd998dad4c0adab9acfacb70771ac9bf885
Brian Fjeldstad <bfjelds@microsoft.com>
2025-11-08 02:40:02 +0000

    use helper for logs handling

--- tools/storm/scripts/init.go
+++ tools/storm/scripts/init.go
@@ -4,12 +4,10 @@ import (
 	"tridenttools/storm/scripts/acr"
 	"tridenttools/storm/scripts/build_extension_images"
 	"tridenttools/storm/scripts/capture_screenshot"
-	"tridenttools/storm/scripts/display_logs"
 )
 
 var TRIDENT_SCRIPTSETS = []any{
 	&acr.AcrScriptSet{},
 	&build_extension_images.BuildExtensionImagesScriptSet{},
 	&capture_screenshot.CaptureScreenshotScriptSet{},
-	&display_logs.DisplayLogsScriptSet{},
 }
