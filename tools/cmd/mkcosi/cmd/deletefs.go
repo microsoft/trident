73e855da9e65d5c077f22279ce9ab64aae8abec0
Paco <frhuelsz@microsoft.com>
2025-11-12 17:18:29 -0800

    Add delete-fs subcommand to mkcosi

---
+++ tools/cmd/mkcosi/cmd/deletefs.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"tridenttools/cmd/mkcosi/builder"
+	"tridenttools/cmd/mkcosi/cosi"
+	"tridenttools/cmd/mkcosi/metadata"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type DeleteFs struct {
+	Source      string   `arg:"" help:"Path to the COSI file to read metadata from." type:"existingfile" required:""`
+	Output      string   `arg:"" help:"Path to write the new COSI file to." type:"path" required:""`
+	FsMntPoints []string `arg:"" help:"Mount point(s) of the target filesystem(s)." required:""`
+}
+
+func (r *DeleteFs) Run() error {
+	log.WithField("source", r.Source).Info("Reading metadata from COSI file")
+	// Read the cosi file once to get the metadata.
+	cosi, err := cosi.ReadCosiFile(r.Source)
+	if err != nil {
+		return fmt.Errorf("failed to read file: %w", err)
+	}
+	defer cosi.Close()
+
+	// First check that the requested filesystems exist.
+	for _, fsMntPoint := range r.FsMntPoints {
+		fsImg := cosi.ImageForMountPoint(fsMntPoint)
+		if fsImg == nil {
+			return fmt.Errorf("filesystem with mount point '%s' not found in COSI file", fsMntPoint)
+		}
+	}
+
+	// Create a new list with images to KEEP.
+	keepImages := make([]metadata.Image, 0, len(cosi.Metadata.Images))
+	for _, img := range cosi.Metadata.Images {
+		if slices.Contains(r.FsMntPoints, img.MountPoint) {
+			log.WithField("mountpoint", img.MountPoint).Info("Deleting filesystem from COSI")
+			continue
+		}
+		keepImages = append(keepImages, img)
+	}
+
+	// Update the metadata to only include the kept images.
+	cosi.Metadata.Images = keepImages
+
+	// Write the new cosi file.
+	outFile, err := os.Create(r.Output)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %w", err)
+	}
+	defer outFile.Close()
+
+	err = builder.BuildCosi(outFile, &cosi.Metadata)
+	if err != nil {
+		return fmt.Errorf("failed to build COSI file: %w", err)
+	}
+
+	log.WithField("output", r.Output).Info("Successfully wrote new COSI file")
+
+	return nil
+}
--- tools/cmd/mkcosi/main.go
+++ tools/cmd/mkcosi/main.go
@@ -12,6 +12,7 @@ type CLI struct {
 	Build           BuildCmd            `cmd:"" help:"Build a COSI file from existing test images!"`
 	ReadMetadata    cmd.ReadMetadata    `cmd:"" help:"Read metadata from a COSI file."`
 	RandomizeFsUuid cmd.RandomizeFsUuid `cmd:"" help:"Randomize the UUID of the specified filesystems in a COSI file."`
+	DeleteFs        cmd.DeleteFs        `cmd:"" help:"Delete the specified filesystems from a COSI file."`
 	ForceColor      bool                `help:"Force color output." short:"c"`
 	Trace           bool                `help:"Enable trace logging." short:"t"`
 }
