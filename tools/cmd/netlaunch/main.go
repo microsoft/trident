4315cf169a094ac92ccf6e25139723758667a16f
Paco <frhuelsz@microsoft.com>
2025-12-04 06:24:22 +0000

    Turn netlaunch into a library

--- tools/cmd/netlaunch/main.go
+++ tools/cmd/netlaunch/main.go
@@ -4,31 +4,16 @@ Copyright © 2023 Microsoft Corporation
 package main
 
 import (
-	"sync"
-	"tridenttools/pkg/config"
-	"tridenttools/pkg/isopatcher"
-	"tridenttools/pkg/netfinder"
+	"tridenttools/pkg/netlaunch"
 	"tridenttools/pkg/phonehome"
-	stormutils "tridenttools/storm/utils"
 
-	"bytes"
-	"context"
 	"fmt"
-	"net"
-	"net/http"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/fatih/color"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/stmcginnis/gofish/redfish"
-	"gopkg.in/yaml.v2"
-
-	bmclib "github.com/bmc-toolbox/bmclib/v2"
-	"github.com/google/uuid"
 )
 
 var (
@@ -81,279 +66,43 @@ var rootCmd = &cobra.Command{
 		log.SetLevel(log.DebugLevel)
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		// Read the ISO
-		iso, err := os.ReadFile(iso)
-		if err != nil {
-			log.WithError(err).Fatalf("failed to find iso for testing")
-		}
-
 		viper.SetConfigType("yaml")
 		viper.SetConfigFile(netlaunchConfigFile)
 		if err := viper.ReadInConfig(); err != nil {
 			log.WithError(err).Fatal("failed to read configuration file")
 		}
 
-		config := config.NetLaunchConfig{}
+		config := netlaunch.NetLaunchConfig{}
 
 		if err := viper.UnmarshalExact(&config); err != nil {
 			log.WithError(err).Fatal("could not unmarshal configuration")
 		}
 
-		localListenAddress := fmt.Sprintf(":%d", listenPort)
-		listen, err := net.Listen("tcp4", localListenAddress)
-		if err != nil {
-			log.WithError(err).Fatalf("failed to open port listening on %s", localListenAddress)
-		}
+		config.IsoPath = iso
+		config.ListenPort = listenPort
+		config.HostConfigFile = tridentConfigFile
+		config.RemoteAddressFile = remoteAddressFile
+		config.LogstreamFile = backgroundLogstreamFull
+		config.TracestreamFile = traceFile
+		config.ServeDirectory = serveFolder
+		config.CertificateFile = signingCert
+		config.EnableSecureBoot = secureBoot
+		config.WaitForProvisioning = waitForProvisioned
+		config.MaxPhonehomeFailures = maxFailures
 
-		// Find the port we're listening on
-		var announcePort string
-		if config.Netlaunch.AnnouncePort != nil {
-			announcePort = fmt.Sprintf("%d", *config.Netlaunch.AnnouncePort)
-		} else {
-			announcePort = strings.Split(listen.Addr().String(), ":")[1]
-		}
+		err := netlaunch.RunNetlaunch(&config)
 
-		// Do we expect Trident to reach back? If so we need to listen to it.
-		// If we have a specified port, we assume that the intent is that Trident will reach back.
-		enable_phonehome_listening := listenPort != 0
-
-		terminateCtx, terminateFunc := context.WithCancel(context.Background())
-		defer terminateFunc()
-
-		result := make(chan phonehome.PhoneHomeResult)
-		server := &http.Server{}
-
-		// Create the final address that will be announced to the BMC and Trident.
-		var announceIp string
-		if config.Netlaunch.AnnounceIp != nil {
-			// If an IP is specified, use it.
-			announceIp = *config.Netlaunch.AnnounceIp
-		} else {
-			// Otherwise, try to be clever...
-			// We need to find the IP of the local interface that can reach the BMC.
-			log.Warn("No announce IP specified. Attempting to find local IP to announce based on BMC IP.")
-			announceIp, err = netfinder.FindLocalIpForTargetIp(config.Netlaunch.Bmc.Ip)
-			if err != nil {
-				log.WithError(err).Fatalf("failed to find local IP for BMC")
-			}
-		}
-
-		announceAddress := fmt.Sprintf("%s:%s", announceIp, announcePort)
-		log.WithField("address", announceAddress).Info("Announcing address")
-
-		// A flag to record if we've already logged the ISO being fetched by the
-		// BMC. We only want to log this once.
-		var isoFetcheLog sync.Once
-		var isoLogFunc = func(address string) {
-			isoFetcheLog.Do(func() {
-				log.WithField("address", address).Info("BMC has requested the ISO!")
-			})
-		}
-
-		// If we have a Trident config file, we need to patch it into the ISO.
-		if len(tridentConfigFile) != 0 {
-			log.Info("Using Trident config file: ", tridentConfigFile)
-			tridentConfigContents, err := os.ReadFile(tridentConfigFile)
-			if err != nil {
-				log.WithError(err).Fatalf("failed to read Trident config")
-			}
-
-			// Replace NETLAUNCH_HOST_ADDRESS with the address of the netlaunch server
-			tridentConfigContentsStr := strings.ReplaceAll(string(tridentConfigContents), "NETLAUNCH_HOST_ADDRESS", announceAddress)
-
-			trident := make(map[string]interface{})
-			err = yaml.UnmarshalStrict([]byte(tridentConfigContentsStr), &trident)
-			if err != nil {
-				log.WithError(err).Fatalf("failed to unmarshal Trident config")
-			}
-
-			if _, ok := trident["trident"]; !ok {
-				trident["trident"] = make(map[interface{}]interface{})
-			}
-			trident["trident"].(map[interface{}]interface{})["phonehome"] = fmt.Sprintf("http://%s/phonehome", announceAddress)
-			trident["trident"].(map[interface{}]interface{})["logstream"] = fmt.Sprintf("http://%s/logstream", announceAddress)
-
-			tridentConfig, err := yaml.Marshal(trident)
-			if err != nil {
-				log.WithError(err).Fatalf("failed to marshal Trident config")
-			}
-
-			err = isopatcher.PatchFile(iso, "/etc/trident/config.yaml", tridentConfig)
-			if err != nil {
-				log.WithError(err).Fatalf("failed to patch Trident config into ISO")
-			}
-
-			if config.Iso.PreTridentScript != nil {
-				log.Info("Patching in pre-trident script!")
-				err = isopatcher.PatchFile(iso, "/trident_cdrom/pre-trident-script.sh", []byte(*config.Iso.PreTridentScript))
-				if err != nil {
-					log.WithError(err).Fatalf("failed to patch pre-trident script into ISO")
-				}
-			}
-
-			if config.Iso.ServiceOverride != nil {
-				log.Info("Patching Trident service override!")
-				err = isopatcher.PatchFile(iso, "/trident_cdrom/trident-override.conf", []byte(*config.Iso.ServiceOverride))
-				if err != nil {
-					log.WithError(err).Fatalf("failed to patch service override into ISO")
-				}
-			}
-
-			http.HandleFunc("/provision.iso", func(w http.ResponseWriter, r *http.Request) {
-				isoLogFunc(r.RemoteAddr)
-				http.ServeContent(w, r, "provision.iso", time.Now(), bytes.NewReader(iso))
-			})
-
-			// We injected the phonehome & logstream config, so we're expecting Trident to reach back
-			enable_phonehome_listening = true
-		} else {
-			// Otherwise, serve the iso as-is
-			http.HandleFunc("/provision.iso", func(w http.ResponseWriter, r *http.Request) {
-				isoLogFunc(r.RemoteAddr)
-				http.ServeContent(w, r, "provision.iso", time.Now(), bytes.NewReader(iso))
-				terminateFunc()
-			})
-		}
-
-		// If we're expecting Trident to reach back, we need to listen for it.
-		if enable_phonehome_listening {
-			// Set up listening for phonehome
-			phonehome.SetupPhoneHomeServer(result, remoteAddressFile)
-
-			// Set up listening for logstream
-			logstreamFull, err := phonehome.SetupLogstream(backgroundLogstreamFull)
-			if err != nil {
-				log.WithError(err).Fatalf("failed to setup logstream")
-			}
-			defer logstreamFull.Close()
-
-			// Set up listening for tracestream
-			traceFile, err := phonehome.SetupTraceStream(traceFile)
-			if err != nil {
-				log.WithError(err).Fatalf("failed to setup tracestream")
-			}
-			defer traceFile.Close()
-
-		}
-
-		if len(serveFolder) != 0 {
-			http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir(serveFolder))))
-		}
-
-		// Start the HTTP server
-		go server.Serve(listen)
-		log.WithField("address", listen.Addr().String()).Info("Listening...")
-		iso_location := fmt.Sprintf("http://%s/provision.iso", announceAddress)
-
-		// Validate that if file at signingCert exists, it can be read
-		if signingCert != "" {
-			file, err := os.Open(signingCert)
-			if err != nil {
-				log.WithError(err).Fatalf("failed to open signing certificate for reading: %s", signingCert)
-			}
-			file.Close()
-		}
-
-		if config.Netlaunch.LocalVmUuid != nil {
-			startLocalVm(*config.Netlaunch.LocalVmUuid, iso_location, secureBoot, signingCert)
-		} else {
-			if config.Netlaunch.Bmc != nil && config.Netlaunch.Bmc.SerialOverSsh != nil {
-				serial, err := config.Netlaunch.Bmc.ListenForSerialOutput()
-				if err != nil {
-					log.WithError(err).Fatalf("Failed to open serial over SSH session")
-				}
-				defer serial.Close()
-			}
-			// Deploy ISO to BMC
-
-			// Default to port 443
-			port := "443"
-			if config.Netlaunch.Bmc.Port != nil {
-				port = *config.Netlaunch.Bmc.Port
-			}
-
-			client := bmclib.NewClient(
-				config.Netlaunch.Bmc.Ip,
-				config.Netlaunch.Bmc.Username,
-				config.Netlaunch.Bmc.Password,
-				bmclib.WithRedfishPort(port),
-			)
-
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-			defer cancel()
-
-			log.Info("Connecting to BMC")
-			client.Registry.Drivers = client.Registry.For("gofish")
-			if err := client.Open(context.Background()); err != nil {
-				log.WithError(err).Fatalf("failed to open connection to BMC")
-			}
-
-			log.Info("Shutting down machine")
-			if _, err = client.SetPowerState(ctx, "off"); err != nil {
-				log.WithError(err).Fatalf("failed to turn off machine")
-			}
-
-			log.WithField("url", iso_location).Info("Setting virtual media to ISO")
-			if _, err = client.SetVirtualMedia(ctx, string(redfish.CDMediaType), iso_location); err != nil {
-				log.WithError(err).Fatalf("failed to set virtual media")
-			}
-
-			log.Info("Setting boot media")
-			if _, err = client.SetBootDevice(ctx, "cdrom", false, true); err != nil {
-				log.WithError(err).Fatalf("failed to set boot media")
-			}
-
-			log.Info("Turning on machine")
-			if _, err = client.SetPowerState(ctx, "on"); err != nil {
-				log.WithError(err).Fatalf("failed to turn on machine")
-			}
-		}
-
-		log.Info("ISO deployed!")
-
-		if len(tridentConfigFile) != 0 {
-			log.Info("Waiting for phone home...")
-		}
-
-		// Wait for something to happen
-		var exitCode = phonehome.ListenLoop(terminateCtx, result, waitForProvisioned, maxFailures)
-
-		err = server.Shutdown(context.Background())
-		if err != nil {
-			log.WithError(err).Errorln("failed to shutdown server")
-		}
+		// Get an exit code based on the error and log it
+		var exitCode int = phonehome.GetExitCodeFromErrorAndLog(err)
 
 		fmt.Printf("Phone home exited: %d\n", exitCode)
 		if onlyPrintExitCode {
+			// Only print the exit code and exit
 			return
 		}
 
 		os.Exit(exitCode)
 	},
-}
-
-func startLocalVm(localVmUuidStr string, isoLocation string, secureBoot bool, signingCert string) {
-	log.Info("Using local VM")
-
-	// TODO: Parse the UUID directly when reading the config file
-	vmUuid, err := uuid.Parse(localVmUuidStr)
-	if err != nil {
-		log.WithError(err).Fatalf("failed to parse LocalVmUuid as UUID")
-	}
-
-	vm, err := stormutils.InitializeVm(vmUuid)
-	if err != nil {
-		log.WithError(err).Fatalf("failed to initialize VM")
-	}
-	defer vm.Disconnect()
-
-	if err = vm.SetFirmwareVars(isoLocation, secureBoot, signingCert); err != nil {
-		log.WithError(err).Fatalf("failed to set UEFI variables")
-	}
-
-	if err = vm.Start(); err != nil {
-		log.WithError(err).Fatalf("failed to start VM")
-	}
 }
 
 func init() {
---
+++ tools/pkg/netlaunch/netlaunch.go
@@ -0,0 +1,291 @@
+package netlaunch
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
+	"tridenttools/pkg/isopatcher"
+	"tridenttools/pkg/netfinder"
+	"tridenttools/pkg/phonehome"
+	stormutils "tridenttools/storm/utils"
+
+	"github.com/bmc-toolbox/bmclib/v2"
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+	"github.com/stmcginnis/gofish/redfish"
+	"gopkg.in/yaml.v2"
+)
+
+func RunNetlaunch(config *NetLaunchConfig) error {
+	// Read the ISO
+	iso, err := os.ReadFile(config.IsoPath)
+	if err != nil {
+		log.WithError(err).Fatalf("failed to find iso for testing")
+	}
+
+	localListenAddress := fmt.Sprintf(":%d", config.ListenPort)
+	listen, err := net.Listen("tcp4", localListenAddress)
+	if err != nil {
+		return fmt.Errorf("failed to open port listening on '%s': %v", localListenAddress, err)
+	}
+
+	// Find the port we're listening on
+	var announcePort string
+	if config.Netlaunch.AnnouncePort != nil {
+		announcePort = fmt.Sprintf("%d", *config.Netlaunch.AnnouncePort)
+	} else {
+		announcePort = strings.Split(listen.Addr().String(), ":")[1]
+	}
+
+	// Do we expect Trident to reach back? If so we need to listen to it.
+	// If we have a specified port, we assume that the intent is that Trident will reach back.
+	enable_phonehome_listening := config.ListenPort != 0
+
+	terminateCtx, terminateFunc := context.WithCancel(context.Background())
+	defer terminateFunc()
+
+	result := make(chan phonehome.PhoneHomeResult)
+	server := &http.Server{}
+
+	// Create the final address that will be announced to the BMC and Trident.
+	var announceIp string
+	if config.Netlaunch.AnnounceIp != nil {
+		// If an IP is specified, use it.
+		announceIp = *config.Netlaunch.AnnounceIp
+	} else {
+		// Otherwise, try to be clever...
+		// We need to find the IP of the local interface that can reach the BMC.
+		log.Warn("No announce IP specified. Attempting to find local IP to announce based on BMC IP.")
+		announceIp, err = netfinder.FindLocalIpForTargetIp(config.Netlaunch.Bmc.Ip)
+		if err != nil {
+			return fmt.Errorf("failed to find local IP for BMC: %v", err)
+		}
+	}
+
+	announceAddress := fmt.Sprintf("%s:%s", announceIp, announcePort)
+	log.WithField("address", announceAddress).Info("Announcing address")
+
+	// A flag to record if we've already logged the ISO being fetched by the
+	// BMC. We only want to log this once.
+	var isoFetcheLog sync.Once
+	var isoLogFunc = func(address string) {
+		isoFetcheLog.Do(func() {
+			log.WithField("address", address).Info("BMC has requested the ISO!")
+		})
+	}
+
+	// If we have a Trident config file, we need to patch it into the ISO.
+	if len(config.HostConfigFile) != 0 {
+		log.Info("Using Trident config file: ", config.HostConfigFile)
+		tridentConfigContents, err := os.ReadFile(config.HostConfigFile)
+		if err != nil {
+			return fmt.Errorf("failed to read Trident config: %v", err)
+		}
+
+		// Replace NETLAUNCH_HOST_ADDRESS with the address of the netlaunch server
+		tridentConfigContentsStr := strings.ReplaceAll(string(tridentConfigContents), "NETLAUNCH_HOST_ADDRESS", announceAddress)
+
+		trident := make(map[string]interface{})
+		err = yaml.UnmarshalStrict([]byte(tridentConfigContentsStr), &trident)
+		if err != nil {
+			return fmt.Errorf("failed to unmarshal Trident config: %v", err)
+		}
+
+		if _, ok := trident["trident"]; !ok {
+			trident["trident"] = make(map[interface{}]interface{})
+		}
+		trident["trident"].(map[interface{}]interface{})["phonehome"] = fmt.Sprintf("http://%s/phonehome", announceAddress)
+		trident["trident"].(map[interface{}]interface{})["logstream"] = fmt.Sprintf("http://%s/logstream", announceAddress)
+
+		tridentConfig, err := yaml.Marshal(trident)
+		if err != nil {
+			return fmt.Errorf("failed to marshal Trident config: %v", err)
+		}
+
+		err = isopatcher.PatchFile(iso, "/etc/trident/config.yaml", tridentConfig)
+		if err != nil {
+			return fmt.Errorf("failed to patch Trident config into ISO: %v", err)
+		}
+
+		if config.Iso.PreTridentScript != nil {
+			log.Info("Patching in pre-trident script!")
+			err = isopatcher.PatchFile(iso, "/trident_cdrom/pre-trident-script.sh", []byte(*config.Iso.PreTridentScript))
+			if err != nil {
+				return fmt.Errorf("failed to patch pre-trident script into ISO: %v", err)
+			}
+		}
+
+		if config.Iso.ServiceOverride != nil {
+			log.Info("Patching Trident service override!")
+			err = isopatcher.PatchFile(iso, "/trident_cdrom/trident-override.conf", []byte(*config.Iso.ServiceOverride))
+			if err != nil {
+				return fmt.Errorf("failed to patch service override into ISO: %v", err)
+			}
+		}
+
+		http.HandleFunc("/provision.iso", func(w http.ResponseWriter, r *http.Request) {
+			isoLogFunc(r.RemoteAddr)
+			http.ServeContent(w, r, "provision.iso", time.Now(), bytes.NewReader(iso))
+		})
+
+		// We injected the phonehome & logstream config, so we're expecting Trident to reach back
+		enable_phonehome_listening = true
+	} else {
+		// Otherwise, serve the iso as-is
+		http.HandleFunc("/provision.iso", func(w http.ResponseWriter, r *http.Request) {
+			isoLogFunc(r.RemoteAddr)
+			http.ServeContent(w, r, "provision.iso", time.Now(), bytes.NewReader(iso))
+			terminateFunc()
+		})
+	}
+
+	// If we're expecting Trident to reach back, we need to listen for it.
+	if enable_phonehome_listening {
+		// Set up listening for phonehome
+		phonehome.SetupPhoneHomeServer(result, config.RemoteAddressFile)
+
+		// Set up listening for logstream
+		logstreamFull, err := phonehome.SetupLogstream(config.LogstreamFile)
+		if err != nil {
+			return fmt.Errorf("failed to setup logstream: %v", err)
+		}
+		defer logstreamFull.Close()
+
+		// Set up listening for tracestream
+		traceFile, err := phonehome.SetupTraceStream(config.TracestreamFile)
+		if err != nil {
+			return fmt.Errorf("failed to setup tracestream: %v", err)
+		}
+		defer traceFile.Close()
+
+	}
+
+	if len(config.ServeDirectory) != 0 {
+		http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir(config.ServeDirectory))))
+	}
+
+	// Start the HTTP server
+	go server.Serve(listen)
+	log.WithField("address", listen.Addr().String()).Info("Listening...")
+	iso_location := fmt.Sprintf("http://%s/provision.iso", announceAddress)
+
+	// Validate that if file at signingCert exists, it can be read
+	if config.CertificateFile != "" {
+		file, err := os.Open(config.CertificateFile)
+		if err != nil {
+			return fmt.Errorf("failed to open signing certificate '%s' for reading: %v", config.CertificateFile, err)
+		}
+		file.Close()
+	}
+
+	if config.Netlaunch.LocalVmUuid != nil {
+		err := startLocalVm(*config.Netlaunch.LocalVmUuid, iso_location, config.EnableSecureBoot, config.CertificateFile)
+		if err != nil {
+			return err
+		}
+	} else {
+		if config.Netlaunch.Bmc != nil && config.Netlaunch.Bmc.SerialOverSsh != nil {
+			serial, err := config.Netlaunch.Bmc.ListenForSerialOutput()
+			if err != nil {
+				return fmt.Errorf("failed to open serial over SSH session: %v", err)
+			}
+			defer serial.Close()
+		}
+		// Deploy ISO to BMC
+
+		// Default to port 443
+		port := "443"
+		if config.Netlaunch.Bmc.Port != nil {
+			port = *config.Netlaunch.Bmc.Port
+		}
+
+		client := bmclib.NewClient(
+			config.Netlaunch.Bmc.Ip,
+			config.Netlaunch.Bmc.Username,
+			config.Netlaunch.Bmc.Password,
+			bmclib.WithRedfishPort(port),
+		)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+		defer cancel()
+
+		log.Info("Connecting to BMC")
+		client.Registry.Drivers = client.Registry.For("gofish")
+		if err := client.Open(context.Background()); err != nil {
+			return fmt.Errorf("failed to open connection to BMC: %v", err)
+		}
+
+		log.Info("Shutting down machine")
+		if _, err = client.SetPowerState(ctx, "off"); err != nil {
+			return fmt.Errorf("failed to turn off machine: %v", err)
+		}
+
+		log.WithField("url", iso_location).Info("Setting virtual media to ISO")
+		if _, err = client.SetVirtualMedia(ctx, string(redfish.CDMediaType), iso_location); err != nil {
+			return fmt.Errorf("failed to set virtual media: %v", err)
+		}
+
+		log.Info("Setting boot media")
+		if _, err = client.SetBootDevice(ctx, "cdrom", false, true); err != nil {
+			return fmt.Errorf("failed to set boot media: %v", err)
+		}
+
+		log.Info("Turning on machine")
+		if _, err = client.SetPowerState(ctx, "on"); err != nil {
+			return fmt.Errorf("failed to turn on machine: %v", err)
+		}
+	}
+
+	log.Info("ISO deployed!")
+
+	if len(config.HostConfigFile) != 0 {
+		log.Info("Waiting for phone home...")
+	}
+
+	// Wait for something to happen
+	exitError := phonehome.ListenLoop(terminateCtx, result, config.WaitForProvisioning, config.MaxPhonehomeFailures)
+
+	err = server.Shutdown(context.Background())
+	if err != nil {
+		log.WithError(err).Errorln("failed to shutdown server")
+	}
+
+	if exitError != nil {
+		log.WithError(err).Errorln("phonehome.ListenLoop exited with error")
+		return exitError
+	}
+
+	return nil
+}
+
+func startLocalVm(localVmUuidStr string, isoLocation string, secureBoot bool, signingCert string) error {
+	log.Info("Using local VM")
+
+	// TODO: Parse the UUID directly when reading the config file
+	vmUuid, err := uuid.Parse(localVmUuidStr)
+	if err != nil {
+		return fmt.Errorf("failed to parse LocalVmUuid as UUID: %v", err)
+	}
+
+	vm, err := stormutils.InitializeVm(vmUuid)
+	if err != nil {
+		return fmt.Errorf("failed to initialize VM: %v", err)
+	}
+	defer vm.Disconnect()
+
+	if err = vm.SetFirmwareVars(isoLocation, secureBoot, signingCert); err != nil {
+		return fmt.Errorf("failed to set UEFI variables: %v", err)
+	}
+
+	if err = vm.Start(); err != nil {
+		return fmt.Errorf("failed to start VM: %v", err)
+	}
+
+	return nil
+}
