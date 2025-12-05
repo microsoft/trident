3aea7a18f1888bc444f3b30e153d320341f3f12b
Ayana Yaegashi <ayaegashi@microsoft.com>
2025-12-05 20:50:17 +0000

    add comments and clear spec_old

--- crates/trident/src/engine/runtime_update.rs
+++ crates/trident/src/engine/runtime_update.rs
@@ -59,7 +59,10 @@ pub(crate) fn stage_update(
     );
     state.with_host_status(|hs| {
         hs.servicing_state = ServicingState::RuntimeUpdateStaged;
+        // Update spec inside the Host Status with the newly input Host
+        // Configuration (stored in ctx.spec).
         hs.spec = ctx.spec;
+        // Update spec_old to the previous spec (ctx.spec_old == hs.spec)
         hs.spec_old = ctx.spec_old;
     })?;
     #[cfg(feature = "grpc-dangerous")]
@@ -132,6 +135,7 @@ pub(crate) fn finalize_update(
     state.with_host_status(|hs| {
         hs.servicing_state = ServicingState::Provisioned;
         hs.spec = ctx.spec; // Update spec after call to engine::update_host_configuration()
+        hs.spec_old = Default::default(); // Clear spec_old now that state is Provisioned
     })?;
     #[cfg(feature = "grpc-dangerous")]
     grpc::send_host_status_state(sender, state)?;
