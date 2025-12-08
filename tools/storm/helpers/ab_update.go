57ff13cdb63458586bb0674a3850a12137b9d013
Ayana Yaegashi <ayaegashi@microsoft.com>
2025-12-08 18:03:33 +0000

    update info

--- tools/storm/helpers/ab_update.go
+++ tools/storm/helpers/ab_update.go
@@ -340,7 +340,7 @@ func (h *AbUpdateHelper) triggerTridentUpdate(tc storm.TestCase) error {
 		}
 
 		if out.Status == 0 && strings.Contains(out.Stderr, "Staging of A/B update succeeded") {
-			logrus.Infof("Staging of update 'AbUpdate' succeeded")
+			logrus.Infof("Staging of A/B update succeeded")
 			break
 		}
 
