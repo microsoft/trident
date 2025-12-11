4376327e09edef5a823bec19a29ed87e8b1fcb5f
Paco Huelsz Prince <frhuelsz@microsoft.com>
2025-12-11 13:30:30 -0800

    Fix dependencies

--- crates/trident_cli/build.rs
+++ crates/trident_cli/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../proto/harpoon.proto")?;
+    tonic_prost_build::compile_protos("../../proto/harpoon.proto")?;
     println!("cargo:rerun-if-env-changed=TRIDENT_VERSION");
     Ok(())
 }
