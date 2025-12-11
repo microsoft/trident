25ef1bafb914359bc698c89ec31bcb7d84fdcfc3
Brian Fjeldstad <bfjelds@microsoft.com>
2025-12-11 21:55:23 +0000

    fix logging/tracing for rollback

--- crates/trident/src/main.rs
+++ crates/trident/src/main.rs
@@ -269,6 +269,7 @@ fn setup_logging(
             | Commands::Update { .. }
             | Commands::Commit { .. }
             | Commands::RebuildRaid { .. }
+            | Commands::Rollback { .. }
     ) {
         multilogger.add_logger(BackgroundLog::new(TRIDENT_BACKGROUND_LOG_PATH).into_logger());
     }
@@ -293,6 +294,7 @@ fn setup_tracing(args: &Cli) -> Result<TraceStream, Error> {
             | Commands::Update { .. }
             | Commands::Commit { .. }
             | Commands::RebuildRaid { .. }
+            | Commands::Rollback { .. }
     ) {
         // Set up the trace sender
         let trace_sender = tracestream
