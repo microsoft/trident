1b6565ac45dd679a669b452ac30f1a1b5a8bd03b
Paco Huelsz Prince <frhuelsz@microsoft.com>
2025-12-13 17:34:46 -0800

    Stream tests

--- crates/trident/src/server/support/stream.rs
+++ crates/trident/src/server/support/stream.rs
@@ -41,3 +41,63 @@ impl<T, U: Unpin> AsMut<UnboundedReceiver<T>> for StreamWithLock<T, U> {
         &mut self.inner
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::sync::Arc;
+
+    use tokio::sync::RwLock;
+    use tokio_stream::StreamExt;
+
+    #[tokio::test]
+    async fn poll_next_yields_items() {
+        let (tx, rx) = tokio::sync::mpsc::unbounded_channel::<u32>();
+        let lock = Arc::new(RwLock::new(()));
+        let guard = lock.clone().write_owned().await;
+
+        let mut stream = StreamWithLock::new(rx, guard);
+
+        tx.send(10).unwrap();
+        tx.send(20).unwrap();
+
+        assert_eq!(stream.next().await, Some(10));
+        assert_eq!(stream.next().await, Some(20));
+    }
+
+    #[tokio::test]
+    async fn as_ref_and_as_mut_expose_inner_receiver() {
+        let (tx, rx) = tokio::sync::mpsc::unbounded_channel::<u32>();
+        let lock = Arc::new(RwLock::new(()));
+        let guard = lock.clone().write_owned().await;
+
+        let mut stream = StreamWithLock::new(rx, guard);
+
+        // AsRef
+        assert!(!stream.as_ref().is_closed());
+
+        // AsMut: closing the receiver should make the stream end.
+        stream.as_mut().close();
+        drop(tx);
+
+        assert_eq!(stream.next().await, None);
+    }
+
+    #[tokio::test]
+    async fn lock_is_released_when_stream_is_dropped() {
+        let (_tx, rx) = tokio::sync::mpsc::unbounded_channel::<u32>();
+        let lock = Arc::new(RwLock::new(0u32));
+        let guard = lock.clone().write_owned().await;
+
+        let stream = StreamWithLock::new(rx, guard);
+
+        // While stream exists, the write lock is held.
+        assert!(lock.try_write().is_err());
+
+        drop(stream);
+
+        // After drop, the write lock should be available again.
+        assert!(lock.try_write().is_ok());
+    }
+}
