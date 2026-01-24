11bf0e509c2df77e9b1dd355755eec3e06223f22
Paco Huelsz Prince <frhuelsz@microsoft.com>
2026-01-23 22:49:50 -0800

    Split proto file

--- crates/harpoon/build.rs
+++ crates/harpoon/build.rs
@@ -1,5 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=../../proto/harpoon.proto");
-    tonic_prost_build::compile_protos("../../proto/harpoon.proto")?;
+    // Re-run the build script if any of the proto files change (scan the proto directory)
+    for entry in std::fs::read_dir("../../proto")? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.extension().and_then(|s| s.to_str()) == Some("proto") {
+            println!("cargo:rerun-if-changed={}", path.display());
+        }
+    }
+
+    tonic_prost_build::compile_protos("../../proto/harpoon/v1/harpoon.proto")?;
     Ok(())
 }
