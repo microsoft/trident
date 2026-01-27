954e6820ecf75e9f3b6f61598bd0e6684d5bd4c8
Paco Huelsz Prince <frhuelsz@microsoft.com>
2026-01-26 17:14:59 -0800

    PR comments

--- crates/harpoon/build.rs
+++ crates/harpoon/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Re-run the build script if any of the proto files change (scan the proto directory)
+    // Re-run the build script if the proto file changes
     println!("cargo:rerun-if-changed=../../proto/harpoon/v1/harpoon.proto");
     tonic_prost_build::compile_protos("../../proto/harpoon/v1/harpoon.proto")?;
     Ok(())
