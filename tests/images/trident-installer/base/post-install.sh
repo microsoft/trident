83098103753e6a566b633bd4bdac828aace06725
Brian Fjeldstad <bfjelds@microsoft.com>
2026-01-28 19:27:00 +0000

    fix post-isntall.sh

--- tests/images/trident-installer/base/post-install.sh
+++ tests/images/trident-installer/base/post-install.sh
@@ -3,13 +3,17 @@ mkdir -p /var/log/audit
 # Use more intuitive path for the ISO mount
 ln -s -T /run/initramfs/live /trident_cdrom
 
-# Load the config from the CDROM so that the user can patch it
-if [ ! -d /etc/trident ]; then
-    mkdir /etc/trident
-fi
-ln -s -T /trident_cdrom/trident-config.yaml /etc/trident/config.yaml
+# Allow various purpose installer images to use post-install.sh, including
+# those that do not use Trident config for installation ('no-trident-config').
+if [ "$1" != "no-trident-config" ]; then
+    # Load the config from the CDROM so that the user can patch it
+    if [ ! -d /etc/trident ]; then
+        mkdir /etc/trident
+    fi
+    ln -s -T /trident_cdrom/trident-config.yaml /etc/trident/config.yaml
 
-if [ ! -d /etc/systemd/system/trident-install.service.d ]; then
-    mkdir /etc/systemd/system/trident-install.service.d
+    if [ ! -d /etc/systemd/system/trident-install.service.d ]; then
+        mkdir /etc/systemd/system/trident-install.service.d
+    fi
+    ln -s -T /trident_cdrom/trident-override.conf /etc/systemd/system/trident-install.service.d/override.conf
 fi
-ln -s -T /trident_cdrom/trident-override.conf /etc/systemd/system/trident-install.service.d/override.conf
