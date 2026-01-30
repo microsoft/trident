df7995937b63c4cb3dc516486b138d848b3f3955
Paco Huelsz Prince <frhuelsz@microsoft.com>
2026-01-30 09:54:01 -0800

    Extend max log window

--- crates/trident/src/io_utils/image_streamer.rs
+++ crates/trident/src/io_utils/image_streamer.rs
@@ -7,6 +7,7 @@ use std::{
 
 use anyhow::{bail, Context, Error};
 use log::{debug, trace};
+use zstd::stream::read::Decoder;
 
 use trident_api::primitives::bytes::ByteCount;
 
@@ -25,7 +26,12 @@ where
     R: Read + HashingReader,
 {
     // Instantiate decoder for ZSTD stream
-    let mut decoder = zstd::stream::read::Decoder::new(BufReader::new(&mut reader))?;
+    let mut decoder = Decoder::new(BufReader::new(&mut reader))?;
+
+    // Set the maximum window size to 1 GiB (2^30 bytes) to support large images.
+    decoder
+        .window_log_max(30)
+        .context("Failed to det ZSTD decoder log window to 2^30")?;
 
     if let Some(max_window_log) = max_window_log {
         decoder.window_log_max(max_window_log).with_context(|| {
