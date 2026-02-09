8f68d1a157f85e4426aaec3255c0aa7c1fdebfe5
Paco Huelsz Prince <frhuelsz@microsoft.com>
2026-02-09 14:47:07 -0800

    Update doc comments

--- crates/trident/src/engine/storage/partitioning/raw_mode.rs
+++ crates/trident/src/engine/storage/partitioning/raw_mode.rs
@@ -18,11 +18,10 @@ use osutils::{
 use crate::engine::EngineContext;
 
 /// This function handles the partitioning logic for raw COSI storage mode. In
-/// this mode, we expect exactly one disk with no partitions, and we create a
-/// single partition that takes up the entire disk. This is a simplified flow
-/// that is only used for raw COSI storage, and it allows us to use the same
-/// underlying partitioning code while bypassing the usual validation and
-/// adoption logic that is not relevant in this mode.
+/// this mode, we expect the image to be carrying a raw GPT that we can directly
+/// replicate onto the disk, so instead of doing any kind of intelligent
+/// partition creation based on the Host Configuration, we will just take the
+/// GPT from the image and write it to disk as-is.
 pub(super) fn create_partitions_for_raw_cosi_storage(
     ctx: &mut EngineContext,
     disk: &ResolvedDisk,
