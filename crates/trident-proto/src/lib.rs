f842a0c54b5de234be3c4eced5b69927731f4714
Paco Huelsz Prince <frhuelsz@microsoft.com>
2026-02-19 14:31:31 -0800

    use formatcp instead of duplicating

--- crates/trident-proto/src/lib.rs
+++ crates/trident-proto/src/lib.rs
@@ -3,6 +3,8 @@
 //! This module contains the gRPC definitions for Trident's gRPC API, generated
 //! from the source proto files using Tonic.
 
+use const_format::formatcp;
+
 /// The generated gRPC code for the Trident API v1.
 pub mod v1 {
     tonic::include_proto!("trident.v1");
@@ -17,21 +19,11 @@ pub mod v1preview {
 /// The default socket for the Trident gRPC server in absolute path format.
 pub const TRIDENT_DEFAULT_SOCKET_PATH: &str = "/run/trident/trident.sock";
 
+/// The URI scheme for Unix domain sockets.
+const UNIX_SOCKET_SCHEME: &str = "unix://";
+
 /// The default socket for the Trident gRPC server, in URI format.
-pub const TRIDENT_DEFAULT_SOCKET_URI: &str = "unix:///run/trident/trident.sock";
+pub const TRIDENT_DEFAULT_SOCKET_URI: &str =
+    formatcp!("{UNIX_SOCKET_SCHEME}{TRIDENT_DEFAULT_SOCKET_PATH}");
 
 pub mod logging;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_socket_path_uri() {
-        // The URI MUST be equal to the path prefixed with "unix://".
-        assert_eq!(
-            TRIDENT_DEFAULT_SOCKET_URI,
-            format!("unix://{}", TRIDENT_DEFAULT_SOCKET_PATH)
-        );
-    }
-}
