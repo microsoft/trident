974216e464b190959669a94a464ffaa2624813f7
Paco Huelsz Prince <frhuelsz@microsoft.com>
2026-04-09 13:44:47 -0700

    fmt

--- crates/trident/src/engine/install_index.rs
+++ crates/trident/src/engine/install_index.rs
@@ -76,7 +76,8 @@ mod tests {
     // (full_esp_efi_path, esp_mount_path). The esp_mount_path is what next_install_index expects.
     fn setup_esp_efi_path(mount_point: &Path) -> (PathBuf, PathBuf) {
         let esp_mount_path = PathBuf::from(ESP_MOUNT_POINT_PATH);
-        let esp_efi_path = path::join_relative(mount_point, &esp_mount_path).join(ESP_EFI_DIRECTORY);
+        let esp_efi_path =
+            path::join_relative(mount_point, &esp_mount_path).join(ESP_EFI_DIRECTORY);
         fs::create_dir_all(&esp_efi_path).unwrap();
         (esp_efi_path, esp_mount_path)
     }
