00652251a3c7bb3db813b8dee7f202887d56c35b
bfjelds <bfjelds@microsoft.com>
2026-04-30 14:53:15 -0700

    infra: include arm64 rpms in feed and only build them in trident-ci/trident-pr* (#623)
    # 🔍 Description
     
    arm64 rpms are being built in every pipeline currently. instead, follow
    the pattern we have for amd64 rpms and build them in PR* and CI
    pipelines, then PRERELEASE can pull from the CI feed, RELEASE can pull
    from the PRERELEASE feed.
    
    CI validation of build/publish:
    https://dev.azure.com/mariner-org/ECF/_build/results?buildId=1106430&view=results
    PRERELEASE validation of download/publish:
    https://dev.azure.com/mariner-org/ECF/_build/results?buildId=1106458

--- scripts/extract-binary.sh
+++ scripts/extract-binary.sh
@@ -5,8 +5,26 @@
 
 set -eux
 
+RPM_DIR=$1
+OUTPUT_PATH=$2
+ARCHITECTURE=$3
+
+RPM_ARCH=""
+case "$ARCHITECTURE" in
+  amd64)
+    RPM_ARCH="x86_64"
+    ;;
+  arm64)
+    RPM_ARCH="aarch64"
+    ;;
+  *)
+    echo "Unsupported architecture: $ARCHITECTURE"
+    exit 1
+    ;;
+esac
+
 TMP_DIR=$(mktemp -d)
-RPM=$(find $1 | grep -P 'trident-\d.*\.rpm')
+RPM=$(find $RPM_DIR | grep -P "trident-\d.*\.${RPM_ARCH}\.rpm")
 
 cp "$RPM" "$TMP_DIR/trident.rpm"
 
@@ -14,4 +32,4 @@ pushd "$TMP_DIR"
 rpm2cpio trident.rpm | cpio -idmv
 popd
 
-mv "$TMP_DIR/usr/bin/trident" $2
+mv "$TMP_DIR/usr/bin/trident" $OUTPUT_PATH
